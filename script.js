4fc264635732c465f0570447fd6574e0a563aa4e
Pang_Tara <75725516+Praparat-uanon@users.noreply.github.com>
2021-06-23 18:02:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// --------------------------Sales and Tax Section-------------------------------//
+
+const mybutton = document.querySelector(".btn-Discount");
+const result = document.querySelector(".result-box");
+const closeModal = document.querySelector(".close-modal");
+
+const calDiscount = function () {
+  const fullPrice = document.getElementById("fullprice").value;
+  const discount = document.getElementById("discount").value;
+
+  return Number(fullPrice - (fullPrice * discount) / 100);
+};
+const calTax = function () {
+  const fullPrice = document.getElementById("fullprice").value;
+  const tax = document.getElementById("tax").value;
+
+  const taxAmount = Number(fullPrice * (tax / 100));
+  if (tax === 0 && tax === undefined) {
+    taxAmount = 0;
+  }
+
+  return Number(taxAmount) + calDiscount();
+};
+
+const showResult = function () {
+  document.getElementById("result").innerHTML = "Your total is  " + calTax();
+  result.classList.remove("hidden");
+};
+
+const closeResult = function () {
+  result.classList.add("hidden");
+};
+
+mybutton.addEventListener("click", showResult);
+closeModal.addEventListener("click", closeResult);
